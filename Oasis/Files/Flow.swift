62d03b4ed4dd1141d411e70646b5753c4589ea70
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-04-27 10:16:10 -0400

    screen context

--- Oasis/Files/Flow.swift
+++ Oasis/Files/Flow.swift
@@ -18,9 +18,11 @@ class UnitaryScreenFlow<Store: StoreProtocol>: ScreenFlow<Store.Output> {
 }
 
 protocol ScreenContextType {
-    associatedtype Containment: UIViewController
+    associatedtype Context: UIViewController
+    associatedtype RecursiveContext: ScreenContextType
 
-    var container: Containment { get }
+    var context: Context { get }
+    func makeNextPlacer() -> AnyScreenPlacer<RecursiveContext>
 }
 
 protocol ScreenPlacerType {
@@ -31,13 +33,21 @@ protocol ScreenPlacerType {
 
 struct ModalContext: ScreenContextType {
     
-    let container: UIViewController
+    let context: UIViewController
+    
+    func makeNextPlacer() -> AnyScreenPlacer<ModalContext> {
+        return ModalPlacer(presenting: context).asAnyPlacer()
+    }
     
 }
 
 struct NavigationContext: ScreenContextType {
     
-    let container: UINavigationController
+    let context: UINavigationController
+    
+    func makeNextPlacer() -> AnyScreenPlacer<NavigationContext> {
+        return NavigationPlacer(navigationController: context).asAnyPlacer()
+    }
     
 }
 
@@ -47,7 +57,7 @@ struct ModalPlacer: ScreenPlacerType {
     
     func place(_ viewController: UIViewController) -> ModalContext {
         presenting.present(viewController, animated: true, completion: nil)
-        return ModalContext.init(container: viewController)
+        return ModalContext.init(context: viewController)
     }
     
 }
@@ -58,31 +68,60 @@ struct NavigationPlacer: ScreenPlacerType {
     
     func place(_ viewController: UIViewController) -> NavigationContext {
         navigationController.pushViewController(viewController, animated: true)
-        return NavigationContext.init(container: navigationController)
+        return NavigationContext.init(context: navigationController)
     }
+    
+}
+
+struct RootNavigationPlacer: ScreenPlacerType {
+    
+    private let placeRoot: (UIViewController) -> Void
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController? = nil, placeRoot: @escaping (UIViewController) -> Void) {
+        self.navigationController = navigationController ?? UINavigationController()
+        self.placeRoot = placeRoot
+    }
+    
+    func place(_ viewController: UIViewController) -> NavigationContext {
+        navigationController.set(rootViewController: viewController, animated: false)
+        placeRoot(navigationController)
+        return NavigationContext.init(context: navigationController)
+    }
+    
+}
+
+struct RootTabBarPlacer: ScreenPlacerType {
+    
+    
     
 }
 
 extension ScreenContextType {
     
     func makeModalPlacer() -> AnyScreenPlacer<ModalContext> {
-        return ModalPlacer.init(presenting: container).asAnyPlacer()
+        return ModalPlacer.init(presenting: context).asAnyPlacer()
+    }
+    
+    func makeNavEmbeddedModalPlacer() -> AnyScreenPlacer<NavigationContext> {
+        let modalPlacer = makeModalPlacer()
+        return RootNavigationPlacer(placeRoot: { root in _ = modalPlacer.place(root) }).asAnyPlacer()
     }
     
     func makeNavigationPlacer() -> AnyScreenPlacer<NavigationContext> {
         return AnyScreenPlacer<NavigationContext>.init({ viewController in
             let navigationController = UINavigationController(rootViewController: viewController)
-            self.container.present(navigationController, animated: true, completion: nil)
-            return NavigationContext.init(container: navigationController)
+            self.context.present(navigationController, animated: true, completion: nil)
+            return NavigationContext.init(context: navigationController)
         })
     }
     
 }
 
-extension ScreenContextType where Containment: UINavigationController {
+extension ScreenContextType where Context: UINavigationController {
     
     func makeNavigationPlacer() -> AnyScreenPlacer<NavigationContext> {
-        return NavigationPlacer.init(navigationController: container).asAnyPlacer()
+        return NavigationPlacer.init(navigationController: context).asAnyPlacer()
     }
     
 }
@@ -110,5 +149,11 @@ extension ScreenPlacerType {
     func asAnyPlacer() -> AnyScreenPlacer<ScreenContext> {
         return AnyScreenPlacer<ScreenContext>.init(self)
     }
+    
+}
+
+struct TabBarRootPlacer {
+    
+    
     
 }
---
+++ Oasis/Files/ScreenPlacer.swift
@@ -0,0 +1,14 @@
+////
+////  ScreenPlacer.swift
+////  Oasis
+////
+////  Created by Trevor Beasty on 4/27/19.
+////  Copyright © 2019 Trevor Beasty. All rights reserved.
+////
+//
+//import Foundation
+//
+//protocol ScreenPlacer {
+//    func place()
+//}
+//
